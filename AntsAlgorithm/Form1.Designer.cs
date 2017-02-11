0416c8a8902b14ff7045dbe1d9dc13171bbf8412
Bartek Poniatyszyn <bartek.poniatyszyn@gmail.com>
2017-02-12 00:50:41 +0100

    Update 1.0

---
+++ AntsAlgorithm/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace AntsAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDebug = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.Location = new System.Drawing.Point(12, 443);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(61, 13);
+            this.labelDebug.TabIndex = 0;
+            this.labelDebug.Text = "labelDebug";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "kropka";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "linia";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 465);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.labelDebug);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDebug;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ AntsAlgorithm/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntsAlgorithm
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Point> pointList;
+        private Graphics canvas;
+        private Brush brush;
+        private bool start;
+        private const short radius = 20;
+        private bool pointSelect;
+        private bool drawPoint;
+        private Point lastSel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pointList = new List<Point>();
+            pointList.Add(new Point(139, 107));//home
+            pointList.Add(new Point(575, 225));//food
+
+            canvas = CreateGraphics();
+            brush = new SolidBrush(Color.Black);
+            start = true;
+            pointSelect = false;
+            drawPoint = true;
+
+        }
+
+        private void drawAntPooint(Point point)
+        {
+            if (start)
+            {
+                canvas.FillEllipse(new SolidBrush(Color.Red), pointList[0].X, pointList[0].Y, radius, radius);
+                canvas.FillEllipse(new SolidBrush(Color.Red), pointList[1].X, pointList[1].Y, radius, radius);
+                start = false;
+            }
+            pointList.Add(new Point(point.X, point.Y));
+            canvas.FillEllipse(brush, point.X, point.Y, radius, radius);
+        }
+
+        private void drawLineBeetweenPoints(Point pointA, Point pointB)
+        {
+            canvas.DrawLine(new Pen(Color.Purple, 5), pointA, pointB );
+        }
+
+        private void DebugLabel(String message)
+        {
+            labelDebug.Text = message;
+        }
+
+        public static bool InsideCircle(int xc, int yc, int r, int x, int y)
+        {
+            int dx = xc - x;
+            int dy = yc - y;
+            return dx * dx + dy * dy <= r * r;
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            DebugLabel(e.X + " " + e.Y);
+
+            if (!drawPoint && lastSel != Point.Empty)
+            {
+                drawLineBeetweenPoints(lastSel, new Point(e.X, e.Y));
+                lastSel = Point.Empty;
+            }
+
+            foreach (Point p in pointList)
+            {
+                if (InsideCircle(p.X, p.Y, radius, e.X, e.Y))
+                {
+                    DebugLabel("kropka!!!");
+                    pointSelect = true;
+                    lastSel = p;
+                    drawPoint = false;
+                    break;
+                }
+            }
+
+            if(drawPoint)
+                drawAntPooint(new Point(e.X, e.Y));
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            drawPoint = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            drawPoint = false;
+        }
+
+    }
+}
